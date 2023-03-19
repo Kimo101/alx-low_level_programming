d2d4db9beaa57b3ec9da26f215438005d5a124de
Mirak <graouna.karim@gmail.com>
2023-03-19 05:47:13 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,15 +4,11 @@ int main(void) {
     int i, j;
 
     for (i = 0; i < 10; i++) {
-        for (j = i; j < 10; j++) {
             putchar(i + '0');
-            putchar(j + '0');
-            if (i != 9 || j != 9) {
+            if (i != 9) {
                 putchar(',');
-                putchar(' ');
             }
         }
-    }
     return 0;
 }
 
