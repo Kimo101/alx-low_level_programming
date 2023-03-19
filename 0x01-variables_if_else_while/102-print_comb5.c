18bfa4e9d738ea043fdc80cb077b8b7e4a7df220
Mirak <graouna.karim@gmail.com>
2023-03-19 15:27:29 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 int main() {
     int i, j;
     
-    for (i = 1; i < 100; i++) {
+    for (i = 0; i < 100; i++) {
         for (j = i; j < 100; j++) {
             putchar('0' + i / 10);
             putchar('0' + i % 10);
