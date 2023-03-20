e69dcd2042f1f3b7d8fd579fb1664bf575b4290d
Mirak <graouna.karim@gmail.com>
2023-03-19 23:40:50 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints a combination of numbers
- * 
+ *
  * Return: alwaays success
  */
 
@@ -13,11 +13,11 @@ int main(void)
 	{
 	for (j = i + 1; j <= 99; j++)
 	{
-	putchar(i/10 + '0');
-	putchar(i%10 + '0');
+	putchar(i / 10 + '0');
+	putchar(i % 10 + '0');
 	putchar(' ');
-	putchar(j/10 + '0');
-	putchar(j%10 + '0');
+	putchar(j / 10 + '0');
+	putchar(j % 10 + '0');
 	if (i != 98 || j != 99)
 	{
 	putchar(',');
