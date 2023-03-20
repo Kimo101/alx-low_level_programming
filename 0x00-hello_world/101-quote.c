c4157fffca9ed4f839b318457d35444a30b6937f
Mirak <graouna.karim@gmail.com>
2023-03-20 00:03:39 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,10 @@
 * Return: 0 (Success)
 */
 
-int main(void) {
-  const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(STDERR_FILENO, msg, 59);
-  return (1);
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, msg, 59);
+	return (1);
 }
