4a197a2f8938a55579058f30289eb4702d28b53b
Mirak <graouna.karim@gmail.com>
2023-03-19 23:55:25 -0700

    quote,c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include<unistd.h>
 /**
 * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
-*followed by a newline, to standard error. 
+*followed by a newline, to standard error.
 * Return: 0 (Success)
 */
 
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-return (1);
+int main(void) {
+  char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(2, msg, 38);
+  return (1);
 }
