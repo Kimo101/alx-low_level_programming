85c8019977c5e18454c505d32fad3d99e646566c
Mirak <graouna.karim@gmail.com>
2023-03-22 19:23:21 -0700

    abs value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,17 +2,16 @@
 
 /**
 * _abs - computes the absolute value of an integer
-* 
+* @c : code to be checked
 * Return: absolute value of a num
 */
 
 int _abs(int c)
-
 {
 	if (c < 0)
 	{
 	int abs_val;
-	
+
 	abs_val = c * (-1);
 	return (abs_val);
 	}
