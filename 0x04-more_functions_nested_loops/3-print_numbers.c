8c22bbe4e6272a949376ea166336445b70e365df
Mirak <graouna.karim@gmail.com>
2023-03-25 03:08:33 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0 up to 9
+ * Return: the numbers 0 to 9
+ */
+
+void print_numbers(void)
+
+{
+	int x;
+
+	for (x = 0; x <= 0; x++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
+}
