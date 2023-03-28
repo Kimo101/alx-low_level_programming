1be721d4c1fb16e0abee403cfb6e69ea78ddeb44
Mirak <graouna.karim@gmail.com>
2023-03-28 09:48:33 -0700

    string_rev

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+int _putchar(char c);
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
@@ -17,7 +19,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
-
-int _putchar(char c);
 
 int _atoi(char *s);
