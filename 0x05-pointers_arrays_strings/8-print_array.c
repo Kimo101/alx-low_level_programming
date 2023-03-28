e14a5c7cadee4f0e8f4e46722fe631370aebad46
Mirak <graouna.karim@gmail.com>
2023-03-28 13:20:18 -0700

    element of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,11 @@ void print_array(int *arr, int n)
 	int i;
 	for (i = 0; i < (n - 1); i++)
 	{
-	printf("%d, ", a[i]);
+	printf("%d, ", arr[i]);
 	}
 	if (i == (n - 1))
 	{
-	printf("%d", a[n - 1]);
+	printf("%d", arr[n - 1]);
 	}
 	printf("\n");
 }
